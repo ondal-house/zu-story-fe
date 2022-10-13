bca506be6d11a4857de48c28ebc1ab0f9c55ed19
ypd01018 <ypd01018@naver.com>
2022-10-14 01:37:21 +0900

    styles: lint

--- src/apis/request.ts
+++ src/apis/request.ts
@@ -3,5 +3,6 @@ import axios, { AxiosRequestConfig } from 'axios';
 const request = async (config: AxiosRequestConfig) => {
   try {
     return (await axios(config)).data;
+    // eslint-disable-next-line no-empty
   } catch (error) {}
 };
