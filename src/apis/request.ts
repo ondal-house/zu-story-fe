2f0f6b20ee35ad22a1fda015248e4cf9248e1b67
ypd01018 <ypd01018@naver.com>
2022-10-15 16:59:21 +0900

    d

--- src/apis/request.ts
+++ src/apis/request.ts
@@ -1,8 +1,5 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 const request = async (config: AxiosRequestConfig) => {
-  try {
-    return (await axios(config)).data;
-    // eslint-disable-next-line no-empty
-  } catch (error) {}
+  return (await axios(config)).data;
 };
