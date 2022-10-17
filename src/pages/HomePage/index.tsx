a339c0c4d14166fcdd48725b1a77dc66e6aefd71
ypd01018 <ypd01018@naver.com>
2022-10-18 00:32:22 +0900

    feat: add request sample

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react';
+import API from '../../apis/request';
+
 const HomePage = () => {
+  useEffect(() => {
+    const getSample = async () => {
+      const data = await API.getSample();
+      console.log(data);
+    };
+    getSample();
+  }, []);
+
   return <>홈페이지</>;
 };
 
